66570091cdb1935b0caad0413a4ebed2974cef9b
Alexei <alexei.prudnikov@gmail.com>
2022-11-20 10:29:58 +0300

    Add task 03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
         }
     }
 
-    //Если необходимо, исправьте данный код (задание 2
+    //Если необходимо, исправьте данный код (задание 2)
     // https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
     //    try {
     //        int d = 0;
@@ -38,6 +38,46 @@ public class Main {
         double catchedRes1 = intArray[index] / d;
         System.out.println(catchedRes1);
 
+    }
+
+    //Если необходимо, исправьте данный код (задание 3)
+    // https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+    public static void task03v01() {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void task03v02() {
+        int a = 90;
+        int b = 3;
+        if (b == 0) {
+            throw new ArithmeticException("Деление на 0");
+        }
+        System.out.println(a / b);
+        printSum(23, 234);
+        int[] abc = {1, 2};
+        int index = 3;
+        if (index >= abc.length) {
+            throw new IndexOutOfBoundsException("Выход за пределы массива.")
+        }
+        abc[index] = 9;
+
+    }
+
+    public static void printSum(Integer a, Integer b) throws NullPointerException {
+        System.out.println(a + b);
     }
 
     public static void main(String[] args) {
